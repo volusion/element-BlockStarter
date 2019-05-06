5d39f2dd1b786596bc8c4ab0fa848a0c84ea741c
Cameron Jones <cameron.jones@volusion.com>
2019-05-06 13:28:28 -0500

    refactor: delete dead code

--- src/block.js
+++ src/block.js
@@ -2,18 +2,7 @@ import { defaultConfig } from './blockConfig';
 
 const factory = ({ React, css, classes }) => {
     const block = class extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                count: 0
-            };
-        }
-
         static defaultProps = defaultConfig;
-
-        click = () => {
-            this.setState({ count: ++this.state.count });
-        };
 
         render() {
             return <div className={css(classes.div)}>{this.props.text}</div>;
