ee49837d09ffb6350cdb71498684b7494fc97af0
Cameron Jones <cameron.jones@volusion.com>
2019-07-02 15:11:19 -0500

    refactor(data): simplify getDataProps()

--- src/data.js
+++ src/data.js
@@ -1,6 +1,2 @@
-/* eslint-disable-next-line no-unused-vars */
-const getDataProps = (utils, props) => {
-    return Promise.resolve({});
-};
-
+const getDataProps = () => Promise.resolve();
 export { getDataProps };
