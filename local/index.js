9b94031e5af8a0f730f0b8ad280bc22a5c0cf881
Chris Decker <chris.decker@volusion.com>
2019-10-08 13:15:18 -0500

    chore: Lints local/index.js

--- local/index.js
+++ local/index.js
@@ -1,52 +1,49 @@
-const blockModule = volBlockStarterBlock
+const blockModule = volBlockStarterBlock;
 
-const tenantId = '$YOUR_TENANT_ID'
+const tenantId = '$YOUR_TENANT_ID';
 
 window.ElementSdk.client.configure({
-  tenant: tenantId
+    tenant: tenantId
 });
 
 const globalStyles = {
-  color: {},
-  globalComponents: {},
-  typography: {}
+    color: {},
+    globalComponents: {},
+    typography: {}
 };
 
-const props = { 
-  ...blockModule.defaultConfig,
-  text: 'Custom prop value for local testing'
-}
+const props = {
+    ...blockModule.defaultConfig,
+    text: 'Custom prop value for local testing'
+};
 
 function createBlock() {
-  const ElementPropTypes = window.ElementSdk.ElementPropTypes
-  const Components = window.ElementSdk.Components
+    const ElementPropTypes = window.ElementSdk.ElementPropTypes;
+    const Components = window.ElementSdk.Components;
 
-  const utils = {
-    client: window.ElementSdk.client
-  }
+    const utils = {
+        client: window.ElementSdk.client
+    };
 
-  return blockModule.factory(
-    { React, ElementPropTypes, Components },
-    utils,
-    aphrodite,
-    globalStyles,
-    props
-  ).block;
+    return blockModule.factory(
+        { React, ElementPropTypes, Components },
+        utils,
+        aphrodite,
+        globalStyles,
+        props
+    ).block;
 }
 
 function configureBlock(data) {
-  const block = createBlock()
-  return block({ ...props, data })
-} 
-
-function renderBlock(data) {
-  const block = configureBlock(data)
-  const root = document.getElementById('root')
-  ReactDOM.render(block, root)
+    const block = createBlock();
+    return block({ ...props, data });
 }
 
-window.onload = () => (
-  blockModule
-  .getDataProps(window.ElementSdk, props)
-  .then(renderBlock)
-)
+function renderBlock(data) {
+    const block = configureBlock(data);
+    const root = document.getElementById('root');
+    ReactDOM.render(block, root);
+}
+
+window.onload = () =>
+    blockModule.getDataProps(window.ElementSdk, props).then(renderBlock);
