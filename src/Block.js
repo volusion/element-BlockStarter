0b2a18456a8397df961378080b702b6ff2cb6bb6
Cameron Jones <cameron.jones@volusion.com>
2019-11-19 10:19:39 -0600

    refactor: update starter block for new contract

--- src/Block.js
+++ src/Block.js
@@ -1,9 +1,9 @@
+import React from 'react';
+
 import { defaultConfig } from './configs';
 
-export const StarterBlockFactory = React => {
-    function StarterBlock(props) {
-        return <h1>{props.text}</h1>;
-    }
-    StarterBlock.defaultProps = defaultConfig;
-    return StarterBlock;
-};
+export function StarterBlock(props) {
+    return <h1>{props.text}</h1>;
+}
+
+StarterBlock.defaultProps = defaultConfig;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { createFactory } from '@volusion/element-block-utils';
-import { StarterBlockFactory } from './Block';
-import { getStyles } from './getStyles';
-import { getConfigSchema, defaultConfig } from './configs';
+import { StarterBlock as block } from './Block';
+import { configSchema, defaultConfig } from './configs';
 import { getDataProps } from './getDataProps';
 
-const factory = createFactory(StarterBlockFactory, { getStyles, getConfigSchema });
-
-export { factory, getDataProps, defaultConfig };
+export { block, getDataProps, defaultConfig, configSchema };
