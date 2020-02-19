df7dd2b25cb82aee4d281f66c0435fe3e1012862
Chris Decker <chris.decker@volusion.com>
2020-02-19 13:46:11 -0600

    refactor: Convert Block to arrow function.

--- src/Block.js
+++ src/Block.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { defaultConfig } from './configs';
 
-function Block(props) {
+const Block = props => {
     return <h1>{props.text}</h1>;
-}
+};
 
 Block.defaultProps = defaultConfig;
 
