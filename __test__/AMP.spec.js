13208be76a29eb0e37bc34f4390d63a33b0ae6a2
Chris Decker <chris.decker@volusion.com>
2020-05-05 11:57:56 -0500

    test: Adds new AMP test file.

---
+++ __test__/AMP.spec.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import {
+    mockUtils as utils,
+    joinClasses
+} from '@volusion/element-block-utils/test-utils';
+import { block as Block, defaultConfig } from '../src';
+
+let props;
+describe('The Block when loaded over AMP', () => {
+    beforeEach(() => {
+        props = {
+            data: {},
+            utils: { ...utils, isAmpRequest: true },
+            joinClasses,
+            queryParams: {}
+        };
+    });
+    it('renders without errors', () => {
+        mount(<Block {...props} />);
+    });
+    describe('when there is no custom data', () => {
+        it('should render the block with the default content', () => {
+            const wrapper = mount(<Block {...props} />);
+            expect(wrapper.text()).toBe(defaultConfig.text);
+        });
+    });
+    describe('when given custom data', () => {
+        it('should render the block using the custom data', () => {
+            const customText = 'Custom Block Text';
+            const wrapper = mount(<Block {...props} text={customText} />);
+            expect(wrapper.text()).toBe(customText);
+        });
+    });
+});
